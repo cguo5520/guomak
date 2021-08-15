6b1e14d70cafab8cb803f988223a61ff1c68000c
cg <cguo5520@gmail.com>
2021-08-15 11:51:36 -0700

    dconf gnome install and uninstall

--- INSTALL.sh
+++ INSTALL.sh
@@ -9,4 +9,16 @@ cp -R xkb_guomak/* /usr/share/X11/xkb
 rm /etc/default/keyboard
 cp etc_default_keyboard_guomak /etc/default/keyboard
 
+function gset {
+    gsettings set "org.gnome.${1}" "$2" "$3"
+}
+
+function gsetd {
+    gset "desktop.${1}" "$2" "$3"
+}
+
+# Keyboard
+gsetd input-sources mru-sources "[('xkb', 'us+guomak')]"
+gsetd input-sources sources "[('xkb', 'us+guomak')]"
+
 echo "Success"
--- UNINSTALL.sh
+++ UNINSTALL.sh
@@ -9,4 +9,16 @@ cp -R xkb_clean/* /usr/share/X11/xkb
 rm /etc/default/keyboard
 cp etc_default_keyboard_colemak /etc/default/keyboard
 
+function gset {
+    gsettings set "org.gnome.${1}" "$2" "$3"
+}
+
+function gsetd {
+    gset "desktop.${1}" "$2" "$3"
+}
+
+# Keyboard
+gsetd input-sources mru-sources "[('xkb', 'us+colemak')]"
+gsetd input-sources sources "[('xkb', 'us+colemak')]"
+
 echo "Success"
