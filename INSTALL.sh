ae10927b92cd10a59f194ec836a7636de247a482
cg <cguo5520@gmail.com>
2021-08-16 01:05:04 -0700

    Mostly working

--- INSTALL.sh
+++ INSTALL.sh
@@ -4,7 +4,30 @@
 echo "Installing Guomak"
 
 rm -rf /usr/share/X11/xkb/*
-cp -R xkb_clean_with_guomak/* /usr/share/X11/xkb
+cp -R xkb_guomak/* /usr/share/X11/xkb
+
+rm -rf /usr/share/X11/xkb/symbols
+cp -R xkb_clean_with_guomak/symbols /usr/share/X11/xkb/symbols
+rm /usr/share/X11/xkb/symbols/us
+cp -R xkb_guomak/symbols/us /usr/share/X11/xkb/symbols/us
+
+rm -rf /usr/share/X11/xkb/compat
+cp -R xkb_clean_with_guomak/compat /usr/share/X11/xkb/compat
+
+#find /usr/share/X11/xkb/symbols -name 'a*' -delete
+#find /usr/share/X11/xkb/symbols -name 'b*' -delete
+#find /usr/share/X11/xkb/symbols -name 'c*' -delete
+#find /usr/share/X11/xkb/symbols -name 'd*' -delete
+#find /usr/share/X11/xkb/symbols -name 'e*' -delete
+#find /usr/share/X11/xkb/symbols -name 'f*' -delete
+###find /usr/share/X11/xkb/symbols -name 'g*' -delete
+#find /usr/share/X11/xkb/symbols -name 'h*' -delete
+#find /usr/share/X11/xkb/symbols -name 'i*' -delete
+#find /usr/share/X11/xkb/symbols -name 'j*' -delete
+#find /usr/share/X11/xkb/symbols -name 'k*' -delete
+###find /usr/share/X11/xkb/symbols -name 'l*' -delete
+#find /usr/share/X11/xkb/symbols -name 'm*' -delete
+#find /usr/share/X11/xkb/symbols -name 'n*' -delete
 
 rm /etc/default/keyboard
 cp etc_default_keyboard_guomak /etc/default/keyboard
